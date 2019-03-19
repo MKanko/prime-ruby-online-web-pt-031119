33a6a7958f612427c0c641ae1b57869190acf73e
Mark Kanko <mkanko66@gmail.com>
2019-03-19 03:04:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def is_prime?(number)
+  if !number.integer?
+    false 
+  elsif number < 2
+    false 
+  elsif number == 2
+    true
+  elsif (2..number - 1).each {|int| if number % int == 0}
+    false 
+  else
+    true
+  end
+end 
+    
