2d991b685f51f9a690df6a311420da84c72ed879
Mark Kanko <mkanko66@gmail.com>
2019-03-19 03:57:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,7 @@ require 'pry'
 def prime?(number)
   if number >= 2
     result = (2..number - 1).collect {|int| number % int == 0}
+    binding.pry
     !result.include?(true)
   else
     return false
